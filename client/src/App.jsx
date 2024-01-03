416b0f4f9ad3eccccbbb15a3e7a7ca54ed49b4f3
Malliswari Thungala <malliswari.thungala@gmail.com>
2024-01-03 13:44:47 -0600

    Added Header as common to all components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,12 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-       <div className='text-red-500'>App</div>
+      <Header />
        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
